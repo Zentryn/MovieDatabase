5b5ea305a5f8b51efdd007fbf72c6aef5870d08c
Zentryn <zentryn@gmail.com>
2018-11-17 20:59:01 +0200

    Added authentication (login functionality)

--- application/__init__.py
+++ application/__init__.py
@@ -11,8 +11,28 @@ app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
 from application import views
+
 from application.movies import models
 from application.movies import views
+
+from application.auth import models
+from application.auth import views
+
+# Login
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please log in to use this functionality."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 # Create all needed tables
 db.create_all()
