c95263cda0c919b657e11df8f980397e3622d43c
Zentryn <zentryn@gmail.com>
2018-11-25 22:33:56 +0200

    Added functionality for genre texts and repositioning of preview item

--- application/static/scripts/movie_form.js
+++ application/static/scripts/movie_form.js
@@ -1,8 +1,13 @@
+var movie_preview_item = null;
+
 window.onload = () => {
     var title_input = document.getElementById('input-title');
     var poster_input = document.getElementById('input-poster');
+    var genres_input = document.getElementById('input-genres');
     var preview_item_cover = document.getElementById('movie-preview-item-cover');
     var preview_item_title = document.getElementById('movie-preview-item-title');
+    var preview_item_genres = document.getElementById('movie-preview-item-genres');
+    movie_preview_item = document.getElementById('movie-preview-item');
 
     title_input.onkeyup = () => {
         preview_item_title.innerHTML = getValue(title_input);
@@ -12,7 +17,18 @@ window.onload = () => {
         preview_item_cover.style.backgroundImage = "url('" + getValue(poster_input) + "')";
     }
 
+    genres_input.onkeyup = () => {
+        var genres = getValue(genres_input).split(",");
+        var genres_text = "";
+        for (var i = 0; i < genres.length; i++) {
+            genres_text += genres[i].trim();
+            if (i < genres.length - 1) genres_text += ", "
+        }
+        preview_item_genres.innerHTML = genres_text;
+    }
+
     resizeCovers();
+    rePositionPreviewItem();
 }
 
 /**
@@ -21,4 +37,29 @@ window.onload = () => {
  */
 function getValue(el) {
     return el.value;
+}
+
+/**
+ * Set value of an element
+ * @param {any} el Element whose value should be set
+ * @param {string} val New value for the element
+ */
+function setValue(el, val) {
+    el.value = val;
+}
+
+// Moves movie preview item if the window shrinks to a small size so it doesn't overlap with other elements
+window.onresize = () => {
+    rePositionPreviewItem();
+}
+
+function rePositionPreviewItem() {
+    if (window.innerWidth < 1250) {
+        movie_preview_item.style.position = "relative";
+        movie_preview_item.classList.add('movie-preview-item-left');
+    }
+    else {
+        movie_preview_item.style.position = "absolute";
+        movie_preview_item.classList.remove('movie-preview-item-left');
+    }
 }
