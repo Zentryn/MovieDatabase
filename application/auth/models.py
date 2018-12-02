8848189ee1fed4696b2075445d2a0ebe0a825221
Zentryn <zentryn@gmail.com>
2018-12-02 23:10:08 +0200

    Added roles for users

--- application/auth/models.py
+++ application/auth/models.py
@@ -6,10 +6,12 @@ class User(Base):
 
     username = db.Column(db.String(144), nullable = False)
     password = db.Column(db.String(144), nullable = False)
+    role = db.Column(db.String(144))
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, role):
         self.username = username
         self.password = password
+        self.role = role;
 
     def get_id(self):
         return self.id
@@ -22,3 +24,6 @@ class User(Base):
 
     def is_authenticated(self):
         return True
+
+    def roles(self):
+        return self.role
