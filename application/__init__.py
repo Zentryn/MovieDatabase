781b4f09f67a9a0de6ea4d6eb4fa7effdb3694f2
Zentryn <zentryn@gmail.com>
2018-12-02 23:09:25 +0200

    Added custom login required for user types

--- application/__init__.py
+++ application/__init__.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+from functools import wraps
+from flask_login import current_user
+
 # Configure Database
 if os.environ.get("HEROKU"):
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
@@ -12,6 +15,29 @@ else:
     app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
+
+# Create login required that supports user roles
+def login_required(role="ANY"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+            if not current_user:
+                return login_manager.unauthorized()
+        
+            if not current_user.is_authenticated:
+                return login_manager.unauthorized()
+            
+            unauthorized = False
+
+            if role != "ANY":
+                unauthorized = (current_user.role != role)
+
+            if unauthorized:
+                return login_manager.unauthorized()
+            
+            return fn(*args, **kwargs)
+        return decorated_view
+    return wrapper
 
 from application import views
 
