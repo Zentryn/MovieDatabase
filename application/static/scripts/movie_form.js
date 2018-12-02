4506e71e4c02f708a366adc8a606423d5bd09bc0
Zentryn <zentryn@gmail.com>
2018-12-02 23:14:11 +0200

    Added jquery for easier document ready handling

--- application/static/scripts/movie_form.js
+++ application/static/scripts/movie_form.js
@@ -1,34 +1,53 @@
-var movie_preview_item = null;
+var title_input = null;
+var poster_input = null;
+var backdrop_input = null;
+var background = null;
+var genres_input = null;
+var item_cover = null;
+var item_title = null;
+var item_genres = null;
 
-window.onload = () => {
-    var title_input = document.getElementById('input-title');
-    var poster_input = document.getElementById('input-poster');
-    var genres_input = document.getElementById('input-genres');
-    var preview_item_cover = document.getElementById('movie-preview-item-cover');
-    var preview_item_title = document.getElementById('movie-preview-item-title');
-    var preview_item_genres = document.getElementById('movie-preview-item-genres');
-    movie_preview_item = document.getElementById('movie-preview-item');
+$(document).ready(() => {
+    title_input = document.getElementById('input-title');
+    poster_input = document.getElementById('input-poster');
+    backdrop_input = document.getElementById('input-backdrop');
+    background = document.getElementById('background');
+    genres_input = document.getElementById('input-genres');
+    item_cover = document.getElementById('movie-item-cover');
+    item_title = document.getElementById('movie-item-title');
+    item_genres = document.getElementById('movie-item-genres');
 
     title_input.onkeyup = () => {
-        preview_item_title.innerHTML = getValue(title_input);
+        updateInputs();
     }
 
     poster_input.onkeyup = () => {
-        preview_item_cover.style.backgroundImage = "url('" + getValue(poster_input) + "')";
+        updateInputs();
+    }
+
+    backdrop_input.onkeyup = () => {
+        updateInputs();
     }
 
     genres_input.onkeyup = () => {
-        var genres = getValue(genres_input).split(",");
+        updateInputs();
+    }
+
+    updateInputs();
+});
+
+function updateInputs() {
+    item_title.innerHTML = getValue(title_input);
+    item_cover.style.backgroundImage = "url('" + getValue(poster_input) + "')";
+    background.style.backgroundImage = "url('" + getValue(backdrop_input) + "')";
+
+    var genres = getValue(genres_input).split(",");
         var genres_text = "";
         for (var i = 0; i < genres.length; i++) {
             genres_text += genres[i].trim();
             if (i < genres.length - 1) genres_text += ", "
         }
-        preview_item_genres.innerHTML = genres_text;
-    }
-
-    resizeCovers();
-    rePositionPreviewItem();
+        item_genres.innerHTML = genres_text;
 }
 
 /**
@@ -46,20 +65,4 @@ function getValue(el) {
  */
 function setValue(el, val) {
     el.value = val;
-}
-
-// Moves movie preview item if the window shrinks to a small size so it doesn't overlap with other elements
-window.onresize = () => {
-    rePositionPreviewItem();
-}
-
-function rePositionPreviewItem() {
-    if (window.innerWidth < 1250) {
-        movie_preview_item.style.position = "relative";
-        movie_preview_item.classList.add('movie-preview-item-left');
-    }
-    else {
-        movie_preview_item.style.position = "absolute";
-        movie_preview_item.classList.remove('movie-preview-item-left');
-    }
 }
