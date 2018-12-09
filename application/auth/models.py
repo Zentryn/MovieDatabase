ef33fbd800cb50fa893b403644ec281a718bd628
Zentryn <zentryn@gmail.com>
2018-12-09 18:10:44 +0200

    Added favorite movies for user

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,5 +1,11 @@
 from application import db
 from application.models import Base
+from application.movies.models import Movie
+
+movie_account = db.Table('movie_account', Base.metadata,
+    db.Column('movie_id', db.Integer, db.ForeignKey('movie.id')),
+    db.Column('account_id', db.Integer, db.ForeignKey('account.id'))
+)
 
 class User(Base):
     __tablename__ = "account"
@@ -7,6 +13,7 @@ class User(Base):
     username = db.Column(db.String(144), nullable = False)
     password = db.Column(db.String(144), nullable = False)
     role = db.Column(db.String(144))
+    favorite_movies = db.relationship("Movie", secondary = movie_account)
 
     def __init__(self, username, password, role):
         self.username = username
